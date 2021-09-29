e6ba885232b7a2a4f5569a1ea3bcab7bdf5bb3db
Mats15 <matias2.vornanen@gmail.com>
2021-09-29 18:57:18 +0300

    indentointi ja kommentit

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Book collection
-The main goal of this project was to create a book collection that is stored in the browsers localStorage.
+The main goal of this project was to create a book collection that is stored in a JSON file.
 
-The website is coded using HTML5, CSS3 and Javascript.
+The website is coded using HTML5, CSS3, Javascript, Node.js and JSON.
 
-You can start the project by typing .\index.html into the terminal.
+You can start the project by typing npm start into the terminal and going into your browser and navigating to http://localhost:3000/.
 
 The project website has a book collection where you can click on a book and the books details will appear in the text boxes on the left.
 
@@ -11,6 +11,6 @@ You can then edit the books details and click the Save button to save them or de
 
 To add a new book to your collection, fill in all the details and click the Save New button.
 
-When you save a book with the Save New or Save buttons, the book is saved into localStorage. 
+When you save a book with the Save New or Save buttons, the book is saved into JSON file. 
 
-When the page refreshes, the books remain in localStorage if they are not deleted with the Delete button.
+When the page refreshes, the books remain in the JSON file if they are not deleted with the Delete button.
--- app.js
+++ app.js
@@ -1,118 +1,119 @@
-const http = require('http');
-const express = require('express');
-const path = require('path');
+const http = require("http");
+const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
 const fs = require("fs");
-const bookCollection = require('./bookCollection.json')
+const bookCollection = require("./bookCollection.json");
 app.use(express.json());
 app.use(express.static("express"));
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-//Get list from json file
-app.get('/listBooks', function (req, res) {
-    //console.log("server testi "+ req + "   " + res);
-    fs.readFile( __dirname + "/" + "bookCollection.json", 'utf8', function (err, data) {
-       //console.log( data );
-       res.end( data );
-    });
- })
-//post data to json
- app.post('/listBooks', function (req, res) {
-    // console.log( req.body); //pura json objektiksi
-    // console.log( req.body.body);
-    var newFinalData;
-    let reqdata = JSON.parse( req.body.body ); 
-    if(reqdata.tag=="new"){
-        fs.readFile( __dirname + "/" + "bookCollection.json", 'utf8', function (err, data) {
-            
-            
-            data = JSON.parse( data ); 
-            console.log(Object.keys(data.books).length); 
-            data['books'].push(
-                {
-                "id":Date.now(),//ottaa ajan nykyisen ajan millisekunneissa ideksi
-                "title":reqdata.title,
-                "author":reqdata.author,
-                "description":reqdata.description,
-                }
-                );
-            //console.log( data.books );
-            newFinalData = data;
-            console.log(newFinalData+"sjkldfhklsdhfjsjk");
-            fs.writeFileSync(__dirname + "/" + "bookCollection.json", JSON.stringify(newFinalData,null, "\t"));
+app.use(bodyParser.json()); // For parsing application/json.
+app.use(bodyParser.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded.
+
+//Get list from json file.
+app.get("/listBooks", function (req, res) {
+  fs.readFile(
+    __dirname + "/" + "bookCollection.json",
+    "utf8",
+    function (err, data) {
+      res.end(data);
+    }
+  );
+});
+//Post data to json.
+app.post("/listBooks", function (req, res) {
+  var newFinalData;
+  let reqdata = JSON.parse(req.body.body);
+  if (reqdata.tag == "new") {
+    fs.readFile(
+      __dirname + "/" + "bookCollection.json",
+      "utf8",
+      function (err, data) {
+        data = JSON.parse(data);
+        data["books"].push({
+          id: Date.now(), //Take current time and convert it to milliseconds and make it to id for books.
+          title: reqdata.title,
+          author: reqdata.author,
+          description: reqdata.description,
         });
-     
-     }
-     if(reqdata.tag=="save"){
-        fs.readFile( __dirname + "/" + "bookCollection.json", 'utf8', function (err, data) {
-        //  console.log( data );
-            
-            data = JSON.parse( data );
-            let modifiedData = {
-                "id":reqdata.bookId,
-                "title":reqdata.title,
-                "author":reqdata.author,
-                "description":reqdata.description,
-                };
-                
-            for (let index = 0; index < data.books.length; index++) {
-                if(data.books[index].id==reqdata.bookId){
-                console.log(data.books[index].id);
-                data.books[index]=modifiedData;
-                }
-            }              
-           
-          
-            newFinalData = data;
-            fs.writeFileSync(__dirname + "/" + "bookCollection.json", JSON.stringify(newFinalData,null, "\t"));
-        });
-        
-     }
-     if(reqdata.tag=="delete"){
-        fs.readFile( __dirname + "/" + "bookCollection.json", 'utf8', function (err, data) {
-        //  console.log( data );
-            
-            data = JSON.parse( data );
-            let modifiedData = {
-                "id":reqdata.bookId,
-                "title":reqdata.title,
-                "author":reqdata.author,
-                "description":reqdata.description,
-                };
-                
-            for (let index = 0; index < data.books.length; index++) {
-                if(data.books[index].id==reqdata.bookId){
-                console.log(data.books[index].id);
-                data.books[index]=undefined;
-                data.books.splice(index,1);
-                }
-            }              
-           
-          
-            newFinalData = data;
-            fs.writeFileSync(__dirname + "/" + "bookCollection.json", JSON.stringify(newFinalData,null, "\t"));
-        });
-     
-     }   
- })
-//Get book with id from jso. 
- app.get('/:id', function (req, res) {
-    // First read existing users.
-    fs.readFile( __dirname + "/" + "bookCollection.json", 'utf8', function (err, data) {
-       let books = JSON.parse( data );
-       let book = books["book" + req.params.id] 
-       console.log( book );
-       res.end( JSON.stringify(book));
-    });
- })
-//default URL for website
-app.use('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/index.html'));
-    //__dirname : It will resolve to your project folder.
-  });
+        newFinalData = data;
+        fs.writeFileSync(
+          __dirname + "/" + "bookCollection.json",
+          JSON.stringify(newFinalData, null, "\t")
+        );
+      }
+    );
+  }
+  if (reqdata.tag == "save") {
+    fs.readFile(
+      __dirname + "/" + "bookCollection.json",
+      "utf8",
+      function (err, data) {
+        data = JSON.parse(data);
+        let modifiedData = {
+          id: reqdata.bookId,
+          title: reqdata.title,
+          author: reqdata.author,
+          description: reqdata.description,
+        };
+
+        for (let index = 0; index < data.books.length; index++) {
+          if (data.books[index].id == reqdata.bookId) {
+            data.books[index] = modifiedData;
+          }
+        }
+        newFinalData = data;
+        fs.writeFileSync(
+          __dirname + "/" + "bookCollection.json",
+          JSON.stringify(newFinalData, null, "\t")
+        );
+      }
+    );
+  }
+  if (reqdata.tag == "delete") {
+    fs.readFile(
+      __dirname + "/" + "bookCollection.json",
+      "utf8",
+      function (err, data) {
+        data = JSON.parse(data);
+        let modifiedData = {
+          id: reqdata.bookId,
+          title: reqdata.title,
+          author: reqdata.author,
+          description: reqdata.description,
+        };
+        for (let index = 0; index < data.books.length; index++) {
+          if (data.books[index].id == reqdata.bookId) {
+            data.books[index] = undefined;
+            data.books.splice(index, 1);
+          }
+        }
+        newFinalData = data;
+        fs.writeFileSync(
+          __dirname + "/" + "bookCollection.json",
+          JSON.stringify(newFinalData, null, "\t")
+        );
+      }
+    );
+  }
+});
+//Get book with id from jso.
+app.get("/:id", function (req, res) {
+  // First read existing users.
+  fs.readFile(
+    __dirname + "/" + "bookCollection.json",
+    "utf8",
+    function (err, data) {
+      let books = JSON.parse(data);
+      let book = books["book" + req.params.id];
+      res.end(JSON.stringify(book));
+    }
+  );
+});
+//Default URL for website.
+app.use("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
 const server = http.createServer(app);
 const port = 3000;
-app.listen(3000,function(){
-})
-//server.listen(port);
+app.listen(3000, function () {});
